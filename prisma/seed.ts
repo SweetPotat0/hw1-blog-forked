25e4965c6e77a40a1afc675379d98d5ec35eb204
15bellge <94174826+15bellge@users.noreply.github.com>
2023-04-21 16:08:16 +0300

    create

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,69 +2,120 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
+// const userData: (numberOfUsers: number) => Prisma.UserCreateInput[] = (numberOfUsers) => {
+//     const users: Prisma.UserCreateInput[] = []
+//     for (let i = 1; i <= numberOfUsers; i++) {
+//         users.push(
+//             {
+//                 name: `Alice${i}`,
+//                 email: `alice${i}@prisma.io`,
+//                 posts: {
+//                     create: [
+//                         {
+//                             title: 'Join the Prisma Slack',
+//                             content: 'https://slack.prisma.io',
+//                             published: true,
+//                         },
+//                     ],
+//                 },
+//             })
+//     }    
+//     return users
+// }
+
+const userData:(i: number) => Prisma.UserCreateInput = (i) => {
+    return (
         {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-]
+            name: `Alice${i}`,
+            email: `alice${i}@prisma.io`,
+            posts: {
+                create: [
+                    {
+                        title: 'Join the Prisma Slack',
+                        content: 'https://slack.prisma.io',
+                        published: true,
+                    },
+                ],
+            },
+        })
+}
+// {
+//     name: 'Nilu',
+//     email: 'nilu@prisma.io',
+//     posts: {
+//         create: [
+//             {
+//                 title: 'Follow Prisma on Twitter',
+//                 content: 'https://www.twitter.com/prisma',
+//                 published: true,
+//             },
+//         ],
+//     },
+// },
+// {
+//     name: 'Mahmoud',
+//     email: 'mahmoud@prisma.io',
+//     posts: {
+//         create: [
+//             {
+//                 title: 'Ask a question about Prisma on GitHub',
+//                 content: 'https://www.github.com/prisma/prisma/discussions',
+//                 published: true,
+//             },
+//             {
+//                 title: 'Prisma on YouTube',
+//                 content: 'https://pris.ly/youtube',
+//             },
+//         ],
+//     },
+// },
+
+
+// const creatUserData:  = () => {
+//     for (let i = 0; i < 1000000; i++) {
+//         userData;
+//     }
+// }
 
 async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
-  console.log(`Seeding finished.`)
+    console.log(`Start seeding ...`)
+    // const createMany = await prisma.user.crete({
+    //     data: userData
+    // })
+    //const users:Prisma.UserCreateInput[] = userData(1000000)
+//     const insertQuery = `
+//     WITH users AS (
+//       INSERT INTO users (name, email)
+//       VALUES ${users.map(u => `('${u.name}', '${u.email}')`).join(',\n')}
+//       RETURNING id
+//     ),
+//     posts AS (
+//       INSERT INTO posts (title, content, published, author_id)
+//       SELECT p.title, p.content, p.published, u.id
+//       FROM UNNEST(ARRAY[${users.map(u => u.posts?.create?.map(p => `('${p.title}', '${p.content}', ${p.published})`).join(','))}]) p
+//       JOIN users u ON u.email = '${u.email}'
+//       RETURNING id
+//     )
+//     SELECT * FROM users;
+//   `;
+//   await prisma.$executeRaw(insertQuery);
+    for (let i = 1; i <= 100; i++) {//for (const u of userData) {
+        const user = await prisma.user.create({
+            data: userData(i),
+        })
+        //if (i % 50000 == 0) {
+            console.log(`Created user with id: ${user.id}`)
+        //}
+    }
+    console.log(`Seeding finished.`)
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
